fac646e968e143a7f3fe37f6f91ea19e3f4a65ab
Sreemathi-Muthukrishnan <64546321+Sreemathi-Muthukrishnan@users.noreply.github.com>
2024-08-31 18:45:25 +0530

    search api for pets

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import ReactDOM from 'react-dom';
-import { Pet } from './Pet';
+import ReactDOM from "react-dom";
+import { Pet } from "./Pet";
+import { SearchParams } from "./SearchParams";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Adopt Me!</h1>
-            <Pet name="Stuart" animal="Dog" breed="Golden Retriever" />
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Stuart" animal="Dog" breed="Golden Retriever" />
+      <SearchParams />
+    </div>
+  );
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />)
+root.render(<App />);
