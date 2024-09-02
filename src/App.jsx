e2398ca291cb764c54b700f2011fcd25065e9e92
Sreemathi-Muthukrishnan <64546321+Sreemathi-Muthukrishnan@users.noreply.github.com>
2024-09-02 20:08:53 +0530

    removed unused pet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { Pet } from "./Pet";
 import { SearchParams } from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Stuart" animal="Dog" breed="Golden Retriever" />
       <SearchParams />
     </div>
   );
